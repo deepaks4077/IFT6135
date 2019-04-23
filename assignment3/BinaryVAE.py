59ae4802a73e38690f3dd88c2a9714b6b31ac3d1
Deepak Sharma <deepaks4077@gmail.com>
2019-04-22 20:32:03 -0400

    Fixed log likelihood estimate by fixing bug in ELBO and implementing log pdf of normal

--- assignment3/BinaryVAE.py
+++ assignment3/BinaryVAE.py
@@ -45,7 +45,10 @@ parser.add_argument('--model-path', type=str, default="saved_params/torch_new_bb
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+torch.cuda.manual_seed(args.seed)
 device = torch.device("cuda" if args.cuda else "cpu")
+
 kwargs = {'num_workers': 2, 'pin_memory': True} if args.cuda else {}
 model_filename_suffix = "{}_{}_{}_{}_{}_{}".format(args.batch_size, args.hidden_dim, args.latent_dim, args.epochs, args.no_cuda, abs(args.learning_rate))
 model_filename = "{}_{}".format(args.model_filename_prefix, model_filename_suffix)
@@ -147,80 +150,63 @@ class BinaryVAE(nn.Module):
 
         return x_, z, mu, logvar
 
+    def BCEloss(self, inp, target):
+        return (target.view(-1, 784)*torch.log(inp.view(-1, 784)) + (1 - target.view(-1, 784))*torch.log(1 - inp.view(-1, 784)))
+
     def loss(self, x):
         recon_x, z, mu, logvar = self.forward(x)
         dist = Bernoulli(recon_x.view(-1, 784))
 
-        BCE = F.binary_cross_entropy(recon_x.view(-1, 784), x.view(-1, 784), reduction = 'sum')
+        BCE = self.BCEloss(recon_x.view(-1, 784), x.view(-1, 784)).sum()
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) -  torch.exp(logvar), dim = 1)
 
-        n_elbo = torch.sum(KLD) + BCE
+        negative_elbo = torch.sum(KLD) - BCE
 
-        return n_elbo, mu, logvar
+        return negative_elbo, mu, logvar
 
     def sample(self, n = 64):
         z = torch.randn(n, self.latent_dim).to(device)
         x_recon = self.decode(z)
         return x_recon
 
-def calc_marginal(model, x, zs, q_z):
+def calc_normal_log_pdf(x, mean, variance):
+    return -0.5 * torch.pow(x - mean, 2) * torch.reciprocal(variance) - 0.5 * torch.log(2 * math.pi * variance)
 
-    k = zs.shape[1]
-    batchsize = x.data.shape[0]
+def marginal(model, x, z):
 
-    #q_z = Normal(model.mu.repeat((k,1,1)).view(batchsize, k, -1), torch.exp(0.5*model.logvar).repeat((k, 1, 1)).view(batchsize, k, -1))
-    recon_xs = model.decode(zs)
-
-    p_xz = Bernoulli(recon_xs.view(batchsize, k, -1))
-    xs = x.view(1, batchsize, -1).repeat((k, 1, 1)).view(batchsize, k, -1)
-    log_pxs = p_xz.log_prob(xs).sum(dim = 2)
-
-    p_z = Normal(torch.zeros(batchsize, k, model.latent_dim).to(device), torch.ones(batchsize, k, model.latent_dim).to(device))
-
-    log_prior = p_z.log_prob(zs).sum(dim = 2)
-    log_posterior = q_z.log_prob(zs).sum(dim = 2)
-
-    logsum = log_pxs + log_prior - log_posterior
-
-    #res = torch.log(torch.exp(logsum).sum(dim = 1) / k)
-    print(f"log_prior = {log_prior.shape}, log_posterior = {log_posterior.shape}, logsum = {logsum.shape}, log_pxs = {log_pxs.shape}")
-    res = torch.logsumexp(logsum, dim = 1) - math.log(k)
-
-    return res
-
-def marginal(self, x, k):
-    mu, logvar = self.encode(x)
-
+    k = z.shape[1]
+    mu, logvar = model.encode(x)
     mu = mu.squeeze(0)
     logvar = logvar.squeeze(0)
     std = torch.exp(0.5*logvar)
     batchsize = x.data.shape[0]
     logsums = torch.empty((batchsize, 1)).to(device)
-    q_z = Normal(mu, torch.exp(logvar))
-
-    for i in range(k):  
-        zs = q_z.sample()
-        recon_xs = self.decode(zs)
+    
+    z = z.view(k, batchsize, -1)
+    q_z = Normal(mu, std)
+    
+    zero_mean = torch.zeros(batchsize, model.latent_dim).to(device)
+    one_std = torch.ones(batchsize, model.latent_dim).to(device)
+    p_z = Normal(torch.zeros(batchsize, model.latent_dim).to(device), torch.ones(batchsize, model.latent_dim).to(device))
+    
+    for i in range(k):
+        zs = z[i]
+        recon_xs = model.decode(zs)
         p_xz = Bernoulli(recon_xs.view(batchsize, 784))
 
         xs = x.view(batchsize, 784)
         log_pxs = torch.sum(p_xz.log_prob(xs), dim = 1)
-    
-        p_z = Normal(torch.zeros(batchsize, self.latent_dim).to(device), torch.ones(batchsize, self.latent_dim).to(device))
 
-        log_prior = torch.sum(p_z.log_prob(zs), dim = 1)
-        log_posterior = torch.sum(q_z.log_prob(zs), dim = 1)
+        log_prior = calc_normal_log_pdf(zs, zero_mean, one_std).sum(dim = 1)
+        log_posterior = calc_normal_log_pdf(zs, mu, std).sum(dim = 1)
 
         logsum = log_pxs + log_prior - log_posterior
         logsums = torch.cat((logsums, logsum[:,None]), dim = 1)
 
     logsums = logsums[:,1:]
-    #res = torch.log(torch.exp(logsums).sum(dim = 1) / k)
-    #print(res)
     res = torch.logsumexp(logsums, dim = 1) - math.log(k)
 
     return res
-
 
 def train(model, epoch, data_loader, optimizer, log_interval=10):
     model.train()
@@ -248,7 +234,6 @@ def train(model, epoch, data_loader, optimizer, log_interval=10):
     print('Epoch: {} Average ELBO: {:.4f}'.format(
         epoch, -train_loss / num_samples))
 
-
 def test(model, epoch, batch_size, num_imp_samples, data_loader):
     model.eval()
     test_loss = 0
@@ -262,16 +247,18 @@ def test(model, epoch, batch_size, num_imp_samples, data_loader):
             test_loss += loss.item()
            
             batchsize = data.data.shape[0]
-            q_z = Normal(mu.repeat((num_imp_samples,1,1)).view(batchsize, num_imp_samples, -1), torch.exp(0.5 * logvar).repeat(num_imp_samples, 1, 1).view(batchsize, num_imp_samples, -1))
-            zs = q_z.sample()
+            q_z = Normal(mu, torch.exp(0.5 * logvar))
+            zs = q_z.sample_n(num_imp_samples).view(batchsize, num_imp_samples, -1)
             
-            ll_batch = calc_marginal(model, data, zs, q_z) 
+            ll_batch = marginal(model, data, zs) 
             marginals = marginals + torch.sum(ll_batch).item()
-            print(f"Log Likelihood of minibatch = {ll_batch.mean().item()}")
+            print(f"Minibatch [{i}/{len(data_loader)}] elbo = {-loss.item()/batchsize}, Log Likelihood of minibatch  = {ll_batch.mean().item()}")
             num_samples += data.shape[0]
         
-    print("ELBO = {}, marginal probability = {}".format(-test_loss/num_samples, marginals / num_samples))
+    print("\nELBO = {}, marginal probability = {}\n".format(-test_loss/num_samples, marginals / num_samples))
 
+
+## The following data loader was provided by CW_Huang ""
 def get_data_loader(dataset_location, batch_size):
     URL = "http://www.cs.toronto.edu/~larocheh/public/datasets/binarized_mnist/"
     # start processing
